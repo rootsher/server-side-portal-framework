818584526c47bb88e494254a52db45ed01e1f91e
Konrad Kowalski <rootsher@gmail.com>
2014-07-27 18:24:04 +0200

    Move runService function to WebService. Add comments. Get possibility set port via process.argv.

--- Web/Application.js
+++ Web/Application.js
@@ -6,28 +6,25 @@ var WebService = require('../Architecture/WebService').WebService;
 var MongoAdapter = require('../Adapters/Mongo').MongoAdapter;
 var RedisAdapter = require('../Adapters/Redis').RedisAdapter;
 
+var webServiceInit = new WebService(new CurrentUser());
+var runService = webServiceInit.runWrapper();
+
+// Database adapters.
 var mongodb = new MongoAdapter();
 var redis = new RedisAdapter();
-
-var webServiceRun;
-
-var runService = function (name, allowedTypeAccount, params) {
-	return function (req, res) {
-		params.req = req;
-		params.res = res;
-		webServiceRun(name, allowedTypeAccount, params);
-	};
-};
 
 app.use(mongodb.connect());
 app.use(redis.connect());
 
 app.use(function (req, res, next) {
-	var webServiceInit = new WebService(new CurrentUser());
-
+	// Add things to dependencies in everything service.
 	webServiceInit.addDependency('mongo', mongodb);
 	webServiceInit.addDependency('redis', redis);
-	webServiceRun = webServiceInit.run.bind(webServiceInit);
+	next();
+});
+
+app.use(function (req, res, next) {
+	webServiceInit.runRef = webServiceInit.run.bind(webServiceInit);
 	next();
 });
 
@@ -39,5 +36,6 @@ app.use(express.static('Resources'));
 app.get('/login', runService('loginGet', [0, 1, 2, 3], { param1: 'value1', param2: 'value2' }));
 app.get('/secret', runService('secretGet', [2, 3], { param1: 'value1', param2: 'value2' }));
 
-app.listen(4444);
-console.log('Server listening on port 4444.');
+
+app.listen(process.argv[2]);
+console.log('Server listening on port ' + process.argv[2] + '.');
