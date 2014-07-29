b5e04ed12e9052250f74522caa8a541fd16c7864
Konrad Kowalski <rootsher@gmail.com>
2014-07-29 19:32:26 +0200

    Add module.exports with login and register property.

--- ServiceDirectories/Web/Public.js
+++ ServiceDirectories/Web/Public.js
@@ -1,6 +1,11 @@
 var path = '../../Web/Handlers/';
 
 module.exports.loginGet = require(path + 'login.get.js').loginGet;
+module.exports.loginPost = require(path + 'login.post.js').loginPost;
+
+module.exports.registerGet = require(path + 'register.get.js').registerGet;
+module.exports.registerPost = require(path + 'register.post.js').registerPost;
+
 module.exports.secretGet = require(path + 'secret.get.js').secretGet;
 
 module.exports.accessDenied = require(path + 'accessDenied.get.js').accessDenied;
