c0a879a8512c8955bf1365b401ac80439ca0ae1e
Konrad Kowalski <rootsher@gmail.com>
2014-08-02 15:35:46 +0200

    Create cookie parser middleware and request context.

--- Web/Application.js
+++ Web/Application.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 var CurrentUser = require('../Architecture/CurrentUser').CurrentUser;
@@ -7,7 +8,8 @@ var WebService = require('../Architecture/WebService').WebService;
 var MongoAdapter = require('../Adapters/Mongo').MongoAdapter;
 var RedisAdapter = require('../Adapters/Redis').RedisAdapter;
 
-var webServiceInit = new WebService(new CurrentUser());
+var requestContext = new CurrentUser();
+var webServiceInit = new WebService(requestContext);
 var runService = webServiceInit.runWrapper();
 
 // Database adapters.
@@ -16,6 +18,10 @@ var redis = new RedisAdapter();
 
 app.use(mongodb.connect());
 app.use(redis.connect());
+
+app.use(cookieParser());
+
+app.use(requestContext.checkCookie(mongodb, redis));
 
 app.use(function (req, res, next) {
 	// Add things to dependencies in everything service.
@@ -41,7 +47,7 @@ app.post('/login', runService('loginPost', [0, 1, 2, 3]));
 app.get('/register', runService('registerGet', [0, 1, 2, 3]));
 app.post('/register', runService('registerPost', [0, 1, 2, 3]));
 
-app.get('/secret', runService('secretGet', [2, 3], { param1: 'value1', param2: 'value2' }));
+app.get('/secret', runService('secretGet', [1, 2, 3], { param1: 'value1', param2: 'value2' }));
 
 
 app.listen(process.argv[2]);
