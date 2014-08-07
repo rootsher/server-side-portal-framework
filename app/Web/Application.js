3f1dfdbbfeb68181e5923849cb0b91a8e6ba8d87
Konrad Kowalski <rootsher@gmail.com>
2014-08-07 20:28:04 +0200

    Implement app compositor module.

--- app/Web/Application.js
+++ app/Web/Application.js
@@ -1,56 +1,77 @@
 'use strict';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var app = express();
+var compositor = require('app-compositor');
+var manager = new compositor.CompositionManager();
 
-var CurrentUser = require('../Architecture/CurrentUser').CurrentUser;
-var WebService = require('../Architecture/WebService').WebService;
-var MongoAdapter = require('../Adapters/Mongo').MongoAdapter;
-var RedisAdapter = require('../Adapters/Redis').RedisAdapter;
+function mongodb() {
+    this.is('mongodb');
+    this.provides('mongodb', function (dependencies) {
+        var MongoAdapter = require('../Adapters/Mongo').MongoAdapter;
+        var mongodb = new MongoAdapter();
 
-var requestContext = new CurrentUser();
-var webServiceInit = new WebService(requestContext);
-var runService = webServiceInit.runWrapper();
+        return mongodb.connect();
+    });
+}
 
-// Database adapters.
-var mongodb = new MongoAdapter();
-var redis = new RedisAdapter();
+function redis() {
+    this.is('redis');
+    this.provides('redis', function (dependencies) {
+        var RedisAdapter = require('../Adapters/Redis').RedisAdapter;
+        var redis = new RedisAdapter();
 
-app.use(mongodb.connect());
+        return redis;
+    });
+}
 
-app.use(cookieParser());
+function app() {
+    this.is('app');
+    this.requires('mongodb');
+    this.requires('redis');
+    this.provides('app', function (dependencies) {
+        var express = require('express');
+        var bodyParser = require('body-parser');
+        var cookieParser = require('cookie-parser');
+        var app = express();
+        var CurrentUser = require('../Architecture/CurrentUser').CurrentUser;
+        var WebService = require('../Architecture/WebService').WebService;
 
-app.use(requestContext.checkCookie(mongodb, redis));
+        var requestContext = new CurrentUser();
+        var webServiceInit = new WebService(requestContext);
+        var runService = webServiceInit.runWrapper();
 
-app.use(function (req, res, next) {
-    // Add things to dependencies in everything service.
-    webServiceInit.addDependency('mongo', mongodb);
-    webServiceInit.addDependency('redis', redis);
-    webServiceInit.addDependency('requestContext', requestContext);
-    next();
+        app.use(cookieParser());
+
+        app.use(requestContext.checkCookie(dependencies.mongodb, dependencies.redis));
+
+        app.use(function (req, res, next) {
+            // Add things to dependencies in everything service.
+            webServiceInit.addDependency('mongo', dependencies.mongodb);
+            webServiceInit.addDependency('redis', dependencies.redis);
+            webServiceInit.addDependency('requestContext', requestContext);
+            next();
+        });
+
+        app.use(function (req, res, next) {
+            webServiceInit.runRef = webServiceInit.run.bind(webServiceInit);
+            next();
+        });
+
+        app.use(express.static('Resources'));
+        app.use(bodyParser.urlencoded({ extended: false }));
+
+        var Routing = require('../System/Routing').Routing;
+
+        Routing.get().forEach(function (route) {
+            app[route.method](route.path, runService(route.service, route.permissions, route.params));
+        });
+
+        app.listen(process.argv[2]);
+    });
+}
+
+manager.runModules([mongodb, redis, app]).done(function applicationCompositionFinished(results) {
+    console.log('** Composition finished! The application is now running.');
+    console.log('** Server listening on port ' + process.argv[2] + '.');
+}, function handleCompositionError(error) {
+    console.error('!! Composition error:', error);
 });
-
-app.use(function (req, res, next) {
-    webServiceInit.runRef = webServiceInit.run.bind(webServiceInit);
-    next();
-});
-
-app.use(express.static('Resources'));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// ### Routing ###
-
-app.get('/login', runService('loginGet', [0, 1, 2, 3]));
-app.post('/login', runService('loginPost', [0, 1, 2, 3]));
-
-app.get('/register', runService('registerGet', [0, 1, 2, 3]));
-app.post('/register', runService('registerPost', [0, 1, 2, 3]));
-
-app.get('/secret', runService('secretGet', [1, 2, 3], { param1: 'value1', param2: 'value2' }));
-
-
-app.listen(process.argv[2]);
-console.log('Server listening on port ' + process.argv[2] + '.');
