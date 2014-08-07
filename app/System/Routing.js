d19b2d0a014c320c7643a2826e6fc2495aa19bc6
Konrad Kowalski <rootsher@gmail.com>
2014-08-07 20:29:29 +0200

    Create place where will be added new routes.

---
+++ app/System/Routing.js
@@ -0,0 +1,18 @@
+var Router = require('../Architecture/Router').Router;
+var routerInit = new Router();
+
+
+// ### Routes ###
+
+routerInit.add('get', '/login', 'loginGet', [0, 1, 2, 3]);
+routerInit.add('post', '/login', 'loginPost', [0, 1, 2, 3]);
+
+routerInit.add('get', '/register', 'registerGet', [0, 1, 2, 3]);
+routerInit.add('post', '/register', 'registerPost', [0, 1, 2, 3]);
+
+routerInit.add('get', '/secret', 'secretGet', [1, 2, 3], { param1: 'value1', param2: 'value2' });
+
+
+module.exports.Routing = {
+    get: routerInit.get.bind(routerInit)
+};
