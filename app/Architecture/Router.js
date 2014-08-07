bd6d6312295e71b86989f913f31121dbf01bba1b
Konrad Kowalski <rootsher@gmail.com>
2014-08-07 20:29:00 +0200

    Create module for storing routes.

---
+++ app/Architecture/Router.js
@@ -0,0 +1,22 @@
+function Route(method, path, service, permissions, params) {
+    this.method = method;
+    this.path = path;
+    this.service = service;
+    this.permissions = permissions;
+    this.params = params || {};
+}
+
+function Router() {
+    this._routes = [];
+}
+
+Router.prototype.add = function add(method, path, service, permissions, params) {
+    this._routes.push(new Route(method, path, service, permissions, params));
+};
+
+Router.prototype.get = function get() {
+    return this._routes;
+};
+
+
+module.exports.Router = Router;
